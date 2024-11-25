ae5f784d0e94b3726e2803a07fc86186392b4d0d
Conrad Bekondo <conradbekondo@gmail.com>
2024-11-25 01:30:14 +0100

    chore(ci): netlify-2

--- app.config.ts
+++ app.config.ts
@@ -7,7 +7,7 @@ const __dirname = dirname(__filename);
 
 export default defineConfig({
   server: {
-    preset: "netlify",
+    preset: "netlify-edge",
   },
   middleware: "./src/middleware.ts",
   vite: {
