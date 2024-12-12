2fb26007a07d1e25320c1b9fd7b7b18900489095
Conrad Bekondo <conradbekondo@gmail.com>
2024-12-12 21:18:14 +0100

    fix: fix Netlify problem

--- netlify/functions/utils/api-provider.ts
+++ netlify/functions/utils/api-provider.ts
@@ -1,9 +1,8 @@
-import '@netlify/functions';
+import Client from '@/lib/api';
 import { Request } from "express";
 import { userTokenContextKey } from "../middleware/auth";
-import Client from '@/lib/api';
 
-const apiOrigin = String(Netlify.env.get('SCHOLARIS_API_ENV'));
+const apiOrigin = String(process.env['SCHOLARIS_API_ENV']);
 export function provideClient(req: Request) {
     const client = new Client(apiOrigin, { auth: () => req.context[userTokenContextKey] as string | undefined });
     return client;
